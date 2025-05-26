5b57d9e5e26f7e50b02bd251bb07acc9b2477fc9
Tfranchetti <franchettioliveratadeo-sec@instituto-sanpablo.com>
2025-05-26 11:16:11 -0300

    libreria_rich

---
+++ TP7/libreria_math.py
@@ -0,0 +1,13 @@
+from rich import print
+from rich.panel import Panel
+from rich.console import Console
+
+print(Panel.fit("[bold magenta]Hola, soy el Navarrete[/bold magenta]\n"
+                "[green]Hoy les muestro la librería 'rich'![/green]\n\n"
+                "[cyan]Sirve para que la consola se vea facherita y colorida[/cyan]",
+                title="Mi primera librería en su prime"))
+
+print("\n[bold blue]Esto es azul[/bold blue], [bold red]esto es rojo[/bold red], y [bold yellow]esto es amarillo[/bold yellow]")
+
+console = Console()
+console.rule("[bold green]Eso fue todo ¡Gracias!")
